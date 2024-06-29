099ba6626e4b787e5800cb2c04c7b08c8e075784
ginger <26461046+gingerchew@users.noreply.github.com>
2024-06-29 14:59:16 -0400

    Adds necessary dependencies and experimental settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), db()],
+  output: 'hybrid',
+  experimental: {
+    actions: true
+  }
 });
