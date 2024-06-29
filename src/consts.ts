654fda5b6927fab552274a93003d0fe23f0c36fa
ginger <26461046+gingerchew@users.noreply.github.com>
2024-06-29 14:59:44 -0400

    Adds some global constants updates readme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Project E.G.A.F.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const DEFAULT_TITLE = 'Project Everyone Gets A Fandom';
+export const GITHUB_URL = 'https://github.com/gingerchew/project-egaf';
+export const MASTODON_URL = '';
